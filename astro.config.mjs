9d7cdef599bbd79e4ab5f53b46f554a2db246444
Guy Blanchfield <guyblanchfield@gmail.com>
2024-04-06 22:35:01 +0100

    Lighthouse Audit Opportunites, plus added Autoprefixer and HTML minify, and drop console option in vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import frontendistahtmlMinify from "@frontendista/astro-html-minify";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+	integrations: [
+		icon(),
+		frontendistahtmlMinify({
+			// https://github.com/frontendista/astro-html-minify#13-customize-minifiers-options
+			html: {
+				// customise terser options
+				// strip out the comments
+				removeComments: true,
+			},
+		}),
+	],
+	vite: {
+		esbuild: {
+			// uncomment this for production!!
+			// drop: ["console", "debugger"],
+		},
+	},
 });
